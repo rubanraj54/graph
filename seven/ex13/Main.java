2012f4e225c487d49904ab63989b32004d4c5f85
rubanraj54 <rubanraj54@gmail.com>
2016-12-27 14:59:28 +0100

    Graph exercise added

---
+++ seven/ex13/Main.java
@@ -0,0 +1,53 @@
+package exercise.seven.ex13;
+
+import java.util.ArrayList;
+
+import exercise.seven.ex14.ShortestPath;
+
+/**
+ * @author Rubanraj Ravichandran
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		AdjacencyMatricesRW readWrite = new AdjacencyMatricesRW();
+
+		AdjacencyMatrices matrix = readWrite.readMatrix("complex_weighted_graph");
+
+		TypeConversion typeConversion = new TypeConversion();
+
+		ShortestPath shortestPath = new ShortestPath();
+
+		Graph graph = typeConversion.toGraph(matrix);
+
+		// Topological sorting
+		TopologicalSort sort = new TopologicalSort();
+
+		if (matrix.isCyclic()) {
+			System.out.println("Graph is cyclic");
+		} else if (!matrix.isDirected()) {
+			System.out.println("Graph is not directed");
+		} else {
+			System.out.println(sort.doTopologicalSort(graph).toString());
+		}
+
+		// shortest path
+
+		Node startNode = graph.getNode(5);
+
+		Node endNode = graph.getNode(8);
+
+		if (startNode != null && endNode != null) {
+			ArrayList<Node> traversalList = shortestPath.findShortestPath(graph, startNode, endNode);
+
+			for (Node node : traversalList) {
+				System.out.println(node.getNodeName());
+			}
+		} else {
+			System.out.println("Start node or End node not found in the graph");
+		}
+
+	}
+
+}
